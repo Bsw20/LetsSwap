17a4d75e20b272164fce378ad5bb83d565f30bdf
Bsw20 <yaakarpunkin@edu.hse.ru>
2021-01-29 22:04:05 +0300

    install Alamofire

--- LetsSwap/Modules/SignIn&SignUp/AuthService.swift
+++ LetsSwap/Modules/SignIn&SignUp/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 struct AuthService {
     func signUp(signUpModel: SignUpViewModel, completion: @escaping (Result<Bool, Error>) -> Void) {
@@ -22,7 +23,6 @@ struct AuthService {
 //
 //
 //        }
-        
         #warning("All checks")
 
         guard let url = URL(string: "http://92.63.105.87:3000/register") else {return}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LetsSwap' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LetsSwap
+  pod 'Alamofire'
+end
